b58ab0205157b906b97f1c05429755f7aa6a7502
Dr. Takeyuki Ueda <gde00107@nifty.com>
2021-12-15 19:12:09 +0900

    fix path

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ sudo sh -c "cp setting_files/gc /etc/network/interfaces.d/"
 
 # install hostapd_mac80211
 sudo apt-get install hostapd
-sudo sh -c "cp hostapd.conf /etc/hostapd/"
-sudo sh -c "cp hostapd /etc/default/"
+sudo sh -c "cp setting_files/hostapd.conf /etc/hostapd/"
+sudo sh -c "cp setting_files/hostapd /etc/default/"
 
 # install hostapd_r8188eu
 git clone https://github.com/PlantDaddy/hostapd-rtl8188.git /home/pi/hostapd-rt$
@@ -17,7 +17,7 @@ sudo apt install `pwd`/libssl1.0.0_1.0.1t-1+deb8u11_armhf.deb
 
 # install udhcpd
 sudo apt-get install udhcpd
-sudo sh -c "cp udhcpd.conf /etc/default/"
+sudo sh -c "cp setting_files/udhcpd.conf /etc/default/"
 
 # For buster, unmask
 # https://github.com/raspberrypi/documentation/issues/1018
