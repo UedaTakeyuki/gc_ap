0ef0ed8917f0734cad763e1fdc05354175c4245a
Dr. Takeyuki Ueda <gde00107@nifty.com>
2021-12-15 20:48:38 +0900

    fix

--- ap.sh
+++ ap.sh
@@ -1,9 +1,13 @@
 # change wlan1 to ap mode.
 sudo ifdown wlan0
-sudo sudo ifup wlan0=ap
+sudo ifup wlan0=ap
 
 # start hostapd
+# refer https://forums.raspberrypi.com/viewtopic.php?t=209818#p1334646
 sudo systemctl start hostapd
+sudo service hostapd stop
+sleep 5
+sudo service hostapd start
 
 # start udhcpd
 sudo systemctl start udhcpd
@@ -13,11 +17,11 @@ sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sudo sysctl net.ipv4.ip_forward=1
 
 # wlan0 -> eth0
-#sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-#sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-#sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 
 # wlan0 -> ppp0
-sudo iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
-sudo iptables -A FORWARD -i ppp0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o ppp0 -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+#sudo iptables -A FORWARD -i ppp0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -A FORWARD -i wlan0 -o ppp0 -j ACCEPT
